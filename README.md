6fd14b8308bdfcc2288e0c8c17d2212b9c477f31
Alexander Shangin <5kr1p7@gmail.com>
2022-05-06 10:42:23 +0300

    First commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Plex Browser
+
+## Search devices
+* [Shodan](https://www.shodan.io/search?query=%22CherryPy%2F5.1.0%22+%22%2Fhome%22)
+
+## Preparing
+`pip3 install -r requirements.txt`
+
+## Usage
+Get sections (libraries) from device:
+`./plex_browser.py <IP>`
+
+Get videos from section:
+`./plex_browser.py <IP> <SECTION_ID>`
---
+++ plex_browser.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+
+import sys
+import requests
+import xmltodict
+from bs4 import BeautifulSoup
+import logging
+from pprint import pprint
+
+# --------------------------------
+
+"""
+Shodan.io: https://www.shodan.io/search?query=%22CherryPy%2F5.1.0%22+%22%2Fhome%22
+"""
+
+if len(sys.argv) < 2:
+  print('Usage: ' + sys.argv[0] + ' <IP>')
+  sys.exit(2)
+  
+else:
+  if len(sys.argv) == 3:
+    section = sys.argv[2]
+
+url = sys.argv[1]
+types = ['movie', 'show']
+
+#logging.basicConfig(level = logging.DEBUG)
+#logging.StreamHandler(sys.stdout)
+
+class Plex():
+  '''
+  Get info from Plex device.
+  
+  Args:
+    host(str): Device's hostname or IP address
+    port(int): Port (default is 32400)
+  '''
+  def __init__(self, host: str, port: int = 32400):
+    self.host = host
+    self.url = f"{host}:{port}"
+    self.token = self.getToken()
+    self.sections = self.getSections()
+    
+  def getToken(self) -> str:
+    '''Get token from settings page.'''
+    settings_page = requests.get(f"http://{self.host}:8181/settings")
+    settings_soup = BeautifulSoup(settings_page.text, 'html.parser')
+    token_tag =  settings_soup.find('input', {'id': 'pms_token'})
+    
+    return token_tag['value']
+
+  def genUrl(self, path: str) -> str:
+    '''Generate URL with token.'''
+    return f"http://{self.url}{path}?X-Plex-Token={self.token}"
+
+  def getVideoLink(self, data) -> str:
+    '''Generate link to video file.'''
+    if type(data) is not list:
+      if not type(data['Part']) is list:
+        return self.genUrl(data['Part']['@key'])
+    return ''
+  
+  def getSections(self) -> dict:
+    '''Get sections (libraries) from device.'''
+    sections_url = self.genUrl('/library/sections/')
+    response = requests.get(sections_url)
+    if response.status_code != requests.codes.ok:
+      print('Bad status code of request!')
+      exit(1)
+    sections = xmltodict.parse(response.content)['MediaContainer']['Directory']
+    
+    sections_list = {}
+    for directory in sections:
+        if directory['@type'] in types:
+            sections_list[directory['@key']] = {
+                'key': directory['@key'],
+                'type': directory['@type'],
+                'title': directory['@title']
+            }
+    return sections_list
+  
+  def getVideosFromSection(self, section_id: int) -> dict:
+    '''Get videos list from section(library).'''
+    videos_url = self.genUrl('/library/sections/' + str(section_id) + '/all')
+    response = requests.get(videos_url)
+    videos = xmltodict.parse(response.content)
+
+    if not videos['MediaContainer'].get('Video'):
+        return []
+  
+    title = videos['MediaContainer']['@title1'] + ' / ' + videos['MediaContainer']['@title2']
+    videos_list = []
+
+    for video in videos['MediaContainer']['Video']:
+      title = video.get('@title') or 'NO TITLE'
+      videos_list.append({ '_title': title, 'link': self.getVideoLink(video['Media']) })
+    
+    return videos_list
+
+plexDevice = Plex(url)
+
+if len(sys.argv) < 3:
+  pprint(plexDevice.sections)
+
+try:
+  pprint(plexDevice.getVideosFromSection(section))
+except NameError:
+  pass
